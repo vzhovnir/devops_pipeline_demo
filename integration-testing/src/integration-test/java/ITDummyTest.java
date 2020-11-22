b4262303fcf01d9bd42144b92256b36e2dd48b16
Vladislav <vzhovnir@griddynamics.com>
2020-11-22 22:57:35 +0200

    add demo repo

---
+++ integration-testing/src/integration-test/java/ITDummyTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+import static junit.framework.Assert.assertTrue;
+
+/**
+ * @author Zhovnir Vladyslav
+ */
+public class ITDummyTest {
+
+    @Test
+    public void dummyTest() {
+        assertTrue(true);
+    }
+}
---
+++ integration-testing/src/test/java/DummyUnitTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+import static junit.framework.Assert.assertTrue;
+
+/**
+ * @author Vladyslav Zhovnir
+ */
+public class DummyUnitTest {
+
+    @Test
+    public void dummyTest() {
+        assertTrue(true);
+    }
+}
